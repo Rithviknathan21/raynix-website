7766246e1ca2a49b039588875c8fb241f9e65f97
RITHVIKNATHAN <rithviknathan21@gmail.com>
2025-10-04 19:38:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from -tagger";
+import { componentTagger } from "-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/raynix-website/",   // 👈 this is the important addition
   server: {
     host: "::",
     port: 8080,
